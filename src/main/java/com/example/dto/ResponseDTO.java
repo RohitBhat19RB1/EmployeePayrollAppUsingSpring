7818e546b946d7c7ab288e798aee3f5ea3635993
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2021-01-16 19:42:44 +0530

    Added:ResponseDTO and pojo class

---
+++ src/main/java/com/example/dto/ResponseDTO.java
@@ -0,0 +1,27 @@
+package com.example.dto;
+
+public class ResponseDTO {
+    private String message;
+    private Object date;
+
+    public ResponseDTO(String message, Object date) {
+        this.message = message;
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getDate() {
+        return date;
+    }
+
+    public void setDate(Object date) {
+        this.date = date;
+    }    
+}
