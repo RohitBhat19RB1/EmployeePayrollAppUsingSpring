c8f12b90183e73eb18fa786ea602c8e4370227b2
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2021-01-16 12:12:48 +0530

    Added:EmployeePayrollDTO

---
+++ src/main/java/com/example/dto/EmployeePayrollDTO.java
@@ -0,0 +1,20 @@
+package com.example.dto;
+
+public class EmployeePayrollDTO {
+	public String name;
+	public long salary;
+	
+
+	public EmployeePayrollDTO(String name, long salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "name=" +name+":salary="+salary;
+	}
+
+}
