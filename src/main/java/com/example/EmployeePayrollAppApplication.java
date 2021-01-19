f4fda7e6dc337ee3b9fa353d4e94dc19b094056e
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2021-01-19 21:41:47 +0530

    Added:Used @slf4j at class level to activate logging

--- src/main/java/com/example/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/EmployeePayrollAppApplication.java
@@ -3,12 +3,19 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Payroll Application");
+		log.info("Employee Payroll App Started");	
+		System.out.println("Welcome");
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		
 	}
 
 }
