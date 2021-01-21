97d8ecf2c01e1fcab44f35364093f54d79c59853
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2021-01-21 13:50:32 +0530

    Added:DataBase setting as Environment variable

--- src/main/java/com/example/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/EmployeePayrollAppApplication.java
@@ -18,7 +18,8 @@ public class EmployeePayrollAppApplication {
 			
 		log.info("Employee Payroll App Started in {} Environment",
 				context.getEnvironment().getProperty("environment"));
-	
+		log.info("Employee Payroll DB User is {} ",
+				context.getEnvironment().getProperty("spring.datasource.username"));	
 	}
 
 }
