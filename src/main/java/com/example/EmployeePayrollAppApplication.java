805908a41a53dbf751490521b999ec11be1bec07
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2021-01-21 13:00:01 +0530

    Added:Logging the environment from appropriate application property

--- src/main/java/com/example/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/EmployeePayrollAppApplication.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,10 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		log.info("Employee Payroll App Started");	
-		System.out.println("Welcome");
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		
+		ApplicationContext context = SpringApplication
+				.run(EmployeePayrollAppApplication.class, args);
+			
+		log.info("Employee Payroll App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+	
 	}
 
 }
+                                 
